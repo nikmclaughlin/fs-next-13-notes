1de7671139a61337e033b4ceff1c93080c2c8b2a
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2023-12-15 23:56:45 -0800

    pb migs

---
+++ pb_migrations/1702711106_updated_notes.js
@@ -0,0 +1,72 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("05tg2l5fpgc912d")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "m3mvrliu",
+    "name": "title",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "wxfjcwkq",
+    "name": "content",
+    "type": "editor",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "convertUrls": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("05tg2l5fpgc912d")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "m3mvrliu",
+    "name": "Title",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "wxfjcwkq",
+    "name": "Content",
+    "type": "editor",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "convertUrls": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
